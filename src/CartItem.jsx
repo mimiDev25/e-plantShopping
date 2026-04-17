99921d9678b1c068da4577900028397208a0c37b
Miryam Boucard <mimibozark@gmail.com>
2026-04-17 13:59:59 -0400

    added dispatchers but need to fix one error

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,41 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+    cart.forEach(item=>{
+        totalCost += parseFloat(item.cost.replace("$", ""))* item.quantity;
+    })
+    return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+   e.preventDefault();
+   onContinueShopping();
   };
 
-
-
+  const handleCheckoutShopping = () => {
+    alert('Functionality to be added for future reference');
+  };
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1){
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity-1}))
+    } else{
+        dispatch(removeItem(item.name))
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const subtotal = parseFloat(item.cost.replace("$", "")) * item.quantity;
+    return subtotal;
   };
 
   return (
