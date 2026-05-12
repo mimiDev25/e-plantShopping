b5be7a160758048373dd74d49866b6e3597fd2e5
Miryam Boucard <mimibozark@gmail.com>
2026-05-12 11:19:02 -0400

    fixed typo

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -295,7 +295,7 @@ function ProductList({ onHomeClick }) {
                                     alt={plant.name}/>
                                     <div className = "product-title">{plant.name}</div>
                                     <div className="product-description">{plant.description}</div>
-                                    <div className = "[product-cost]">${plant.cost}</div>
+                                    <div className = "product-cost">${plant.cost}</div>
                                     <button
                                     className="product-button"
                                     onClick = {()=>handleAddToCart(plant)}>
