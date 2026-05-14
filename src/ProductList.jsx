9046ce3041a65ddfacb11f89061dba006a69e45a
Miryam Boucard <mimibozark@gmail.com>
2026-05-14 10:29:25 -0400

    added conditional rendering to 'add to cart' button. Added state-based rendering to cart quantity.

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -243,7 +243,7 @@ function ProductList({ onHomeClick }) {
     const styleA = {
         color: 'white',
         fontSize: '30px',
-        textDecoration: 'none',
+        textDecoration: 'none'
     }
 
     const handleHomeClick = (e) => {
@@ -289,7 +289,25 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>`${calculateTotalItems}`</h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            <h1 className='cart'>
+                                <svg xmlns="http://www.w3.org/2000/svg" 
+                                    viewBox="0 0 256 256" 
+                                    id="IconChangeColor" 
+                                    height="68" width="68">
+                                <rect width="156" height="156" fill="none"></rect>
+                                <circle cx="80" cy="216" r="12"></circle>
+                                <circle cx="184" cy="216" r="12"></circle>
+                                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" 
+                                    fill="none" 
+                                    stroke="#faf9f9" 
+                                    stroke-linecap="round" 
+                                    stroke-linejoin="round" 
+                                    stroke-width="2" 
+                                    id="mainIconPathAttribute"></path>
+                                    </svg>
+                                    <span className = "cart_quantity_count">{calculateTotalItems()}</span>
+                                    </h1></a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -314,7 +332,7 @@ function ProductList({ onHomeClick }) {
                                         cart.some(item => item.name === plant.name) ? "product-button added-to-cart" : "product-button"
                                     }
                                     onClick = {()=>handleAddToCart(plant)}>
-                                    Add to Cart
+                                    {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
                                     </button>
                                 </div>
                             ))}
